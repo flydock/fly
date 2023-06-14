ed57abfc294fc0cdce66d43785b16bff08e3a41d
Dmitry Kotlyarov <dmitry.kotlyarov@kubfly.com>
2023-06-14 15:52:26 +0300

    removed github default env vars

--- build.sh
+++ build.sh
@@ -36,29 +36,6 @@ docker build --no-cache --file ./Dockerfile\
                         --build-arg commit_author_email="$COMMIT_AUTHOR_EMAIL"\
                         --build-arg build="$BUILD"\
                         --build-arg build_time="$BUILD_TIME"\
-                        --build-arg github_action="$GITHUB_ACTION"\
-                        --build-arg github_action_repository="$GITHUB_ACTION_REPOSITORY"\
-                        --build-arg github_actor="$GITHUB_ACTOR"\
-                        --build-arg github_base_ref="$GITHUB_BASE_REF"\
-                        --build-arg github_event_name="$GITHUB_EVENT_NAME"\
-                        --build-arg github_event_path="$GITHUB_EVENT_PATH"\
-                        --build-arg github_head_ref="$GITHUB_HEAD_REF"\
-                        --build-arg github_job="$GITHUB_JOB"\
-                        --build-arg github_ref="$GITHUB_REF"\
-                        --build-arg github_ref_name="$GITHUB_REF_NAME"\
-                        --build-arg github_ref_type="$GITHUB_REF_TYPE"\
-                        --build-arg github_repository="$GITHUB_REPOSITORY"\
-                        --build-arg github_repository_owner="$GITHUB_REPOSITORY_OWNER"\
-                        --build-arg github_run_id="$GITHUB_RUN_ID"\
-                        --build-arg github_run_number="$GITHUB_RUN_NUMBER"\
-                        --build-arg github_server_url="$GITHUB_SERVER_URL"\
-                        --build-arg github_sha="$GITHUB_SHA"\
-                        --build-arg github_workflow="$GITHUB_WORKFLOW"\
-                        --build-arg github_workflow_ref="$GITHUB_WORKFLOW_REF"\
-                        --build-arg github_workflow_sha="$GITHUB_WORKFLOW_SHA"\
-                        --build-arg runner_arch="$RUNNER_ARCH"\
-                        --build-arg runner_name="$RUNNER_NAME"\
-                        --build-arg runner_os="$RUNNER_OS"\
                         --tag $GROUP/$APPLICATION:$REVISION .
 
 if [ "$1" != "" ]; then
