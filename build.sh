25d0cd54383135134f2ea8de6697e6692cf5e9b0
Dmitry Kotlyarov <dmitry.kotlyarov@kubfly.com>
2023-08-30 01:14:44 +0300

    removed buildx

--- build.sh
+++ build.sh
@@ -28,28 +28,28 @@ export OS="linux"
 export ARCH="$1"
 export DEPENDENCIES="[]"
 
-podman buildx build --no-cache --file ./Dockerfile --platform $OS/$ARCH\
-                    --build-arg group="$GROUP"\
-                    --build-arg application="$APPLICATION"\
-                    --build-arg version="$VERSION"\
-                    --build-arg revision="$REVISION"\
-                    --build-arg description="$DESCRIPTION"\
-                    --build-arg license="$LICENSE"\
-                    --build-arg company="$COMPANY"\
-                    --build-arg web="$WEB"\
-                    --build-arg repository="$REPOSITORY"\
-                    --build-arg branch="$BRANCH"\
-                    --build-arg commit="$COMMIT"\
-                    --build-arg commit_time="$COMMIT_TIME"\
-                    --build-arg commit_message="$COMMIT_MESSAGE"\
-                    --build-arg commit_author="$COMMIT_AUTHOR"\
-                    --build-arg commit_author_email="$COMMIT_AUTHOR_EMAIL"\
-                    --build-arg build="$BUILD"\
-                    --build-arg build_time="$BUILD_TIME"\
-                    --build-arg os="$OS"\
-                    --build-arg arch="$ARCH"\
-                    --build-arg dependencies="$DEPENDENCIES"\
-                    --tag $GROUP/$APPLICATION:$REVISION .
+podman build --no-cache --file ./Dockerfile --platform $OS/$ARCH\
+                        --build-arg group="$GROUP"\
+                        --build-arg application="$APPLICATION"\
+                        --build-arg version="$VERSION"\
+                        --build-arg revision="$REVISION"\
+                        --build-arg description="$DESCRIPTION"\
+                        --build-arg license="$LICENSE"\
+                        --build-arg company="$COMPANY"\
+                        --build-arg web="$WEB"\
+                        --build-arg repository="$REPOSITORY"\
+                        --build-arg branch="$BRANCH"\
+                        --build-arg commit="$COMMIT"\
+                        --build-arg commit_time="$COMMIT_TIME"\
+                        --build-arg commit_message="$COMMIT_MESSAGE"\
+                        --build-arg commit_author="$COMMIT_AUTHOR"\
+                        --build-arg commit_author_email="$COMMIT_AUTHOR_EMAIL"\
+                        --build-arg build="$BUILD"\
+                        --build-arg build_time="$BUILD_TIME"\
+                        --build-arg os="$OS"\
+                        --build-arg arch="$ARCH"\
+                        --build-arg dependencies="$DEPENDENCIES"\
+                        --tag $GROUP/$APPLICATION:$REVISION .
 
 if [ "$2" != "" ]; then
   podman tag $GROUP/$APPLICATION:$REVISION $2/$GROUP/$APPLICATION:$REVISION
